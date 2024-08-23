0a1212a33adc51365f529554d1c88200f8ee392b
mvttheos <mvttheos@gmail.com>
2024-08-23 02:52:23 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gesmie",
+  title: "Gesmia App",
   description: "Una app de seguridad personal",
 };
 
